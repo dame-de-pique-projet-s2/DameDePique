bd6723ff7c8895fd73eb388b0ea2a98c417fd33d
Flogaux <margaux.biberfeld@iut-rodez.fr>
2019-05-01 20:50:17 +0200

    Rajout de la première methode et de son test pour l'I.A

--- src/damedepique/ia/IA.java
+++ src/damedepique/ia/IA.java
@@ -1,25 +1,80 @@
 /*
- * IA.java                                                           30/04/2019
+ * IA.java                                                           01/05/2019
  * Projet de la dame de pique | IUT de Rodez | 2018 - 2019
  */
 
 package damedepique.ia;
 
 import damedepique.general.Joueur;
+import java.util.ArrayList;
 
 /**
- * TODO Faire la description de la classe IA.
+ * <p>
+ * Cette classe sert à  gérer l'inteligence artificielle qui servira 
+ * d'adversaire au joueur humain.
+ * </p>
+ * <ul>
+ *    <li>
+ *         Pour cela cette classe va :
+ *    </li>
+ *    <li>
+ *            -echanger 3 cartes (pour l'instant dans l'ordre et par la suite 
+ *             de façon plus réfléchie)
+ *    </li>
+ *    <li>
+ *            -jouer les cartes qu'elle a le droit de jouer (pour l'instant 
+ *             dans l'ordre puis par la suite de maniÃ¨re à la faire gagner)
+ *    </li>
+ * </ul>
+ *
+ *
  * @author Julien B.
  * @version 1.0
  */
 public class IA extends Joueur {
 
 	/**
-	 * Création d'une nouvelle IA avec les caractéristiques d'un joueur.
+	 * CrÃ©ation d'une nouvelle IA avec les caractÃ©ristiques d'un joueur.
 	 * @see damedepique.general.Joueur
 	 */
 	public IA() {
 		super();
 	}
 	
+	 /**
+     * mÃ©thode qui choisi 3 cartes Ã  echanger au debut des manches
+	 * Les 3 cartes sont (pour le moment) les 3 premieres de la main
+	 * 
+	 * @return echange[] un tableau de 3 elements contenants des cartes
+     */
+	 public Carte[] carteEchange() {
+		// Création d'un tableau d'objets carte pour stocker les cartes 
+		// choisies pour l'échange
+        Carte[] echange = new  Carte[3];
+  
+        // On donne au tableau les trois premieres cartes de la main triée
+        for (int i = 0; i<3; i++){
+            echange[i]= getMain().get(i);
+        }
+        // On renvoie le tableau contenant les cartes que l'IA va echanger
+        return echange;
+
+    }
+		
+    
+	
+	 // TODO à finir la deuxieme methode
+//	/**
+//	 * mÃ©thode qui joue une carte
+//	 * La carte est (pour le moment) la premiere possible de la main
+//	 *
+//	 * @return aJouer la carte qui vas Ãªtre jouÃ©e
+//	 */
+//	public void jouerCarte() {
+//		Carte aJouer = new Carte;
+//		aJouer = cartesJouables.get(0);
+//		return aJouer		
+//	}
+	
 }
+
--- src/damedepique/ia/tests/TestIA.java
+++ src/damedepique/ia/tests/TestIA.java
@@ -1,24 +1,76 @@
-/*
- * TestIA.java                                                       25/04/2019
+/* TestIA.java                                                       01/05/2019
  * Projet de la dame de pique | IUT de Rodez | 2018 - 2019
  */
 
 package damedepique.ia.tests;
 
+import java.util.Collections;
+
+import damedepique.general.Carte;
+import damedepique.general.Joueur;
+import damedepique.general.Paquet;
+import damedepique.ia.IA;
+
 /**
  * Cette classe contient toutes les méthodes de test de la classe IA.
+ * 
  * @author Julien B.
  * @version 1.0
  */
 public class TestIA {
 	
 	/**
+	 * Test de la méthode carteEchange
+	 */
+	public static void testcarteEchange() {
+		System.out.println("IA.carteEchange()\n"
+	                + "------------------");
+
+	    // Instantiation d'un nouveau paquet de cartes.
+	    Paquet paquet = new Paquet();
+			
+	    // Instantiation et initialisation de quatre joueurs.
+	    Joueur[] joueurs = new Joueur[4];
+	    for (int i = 0 ; i < joueurs.length ; i++) {
+		    joueurs[i] = new IA();
+	    }
+			
+	    // Création du nouveau paquet de cartes.
+	    paquet.creer();
+			
+	    // Mélange du paquet précédemment crée. 
+	    paquet.melanger();
+			
+	    // Distribution du paquet de cartes entre les quatre joueurs.
+	    paquet.distribuer(joueurs);
+	
+	    // Tri de la main pour le joueur I.A du test.
+	    joueurs[0].trierMain();
+	
+	    // Affichage de la main du joueur
+        System.out.println("\n" + joueurs[0]);
+		
+	    // Création du tableau de carte qui contiendra les choix de l'I.A pour l'échange.
+        Carte[] echange = new Carte[3];
+        
+    	echange = ((IA) joueurs[0]).carteEchange();
+    	
+    	// Affichage du résultat donné par la méthode
+	    System.out.println("Cartes à échanger : ");
+    	for (Carte carte : echange) {
+		    System.out.println(carte.toString());
+    	}
+		
+	}
+	
+	
+  	/**
 	 * Lancement des méthodes de test de la classe IA.
 	 * @param args Non utilisé.
 	 */
 	public static void main(String[] args) {
 		
-		
+		testcarteEchange();
 
 	}
 
